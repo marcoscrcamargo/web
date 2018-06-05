6f2053327ced0b565c39ecb6992df81766548c12
gcamargo96 <gacamargo1.000@gmail.com>
2018-06-05 04:25:06 -0300

    Checagem se o usuario está logado para comprar produtos

--- T2/petshop/src/components/Products.jsx
+++ T2/petshop/src/components/Products.jsx
@@ -22,59 +22,86 @@ export default class Products extends React.Component {
 	render() {
 		let products = this.state.products;
 
-		// fills a list with cards with name, image, price and a short description about each product
-		let productList = products.map((prod, index) => {
-				this.prodId = prod.id;
-				this.prodName = prod.name;
-				this.prodImg = prod.img_file;
-				this.prodDescription = prod.description;
-				this.prodPrice = prod.price;
+		if(this.props.user === null){
+			let productList = products.map((prod) => {
+					return (
+						<Col s={6} m={4} l={2} >
+							<Card className='medium' /*type of the card*/
+								header={<CardTitle image={prod.img_file}></CardTitle>} /*adding an image*/
+								actions={<Modal
+									header={prod.name}
+									trigger={<Button>{prod.price} Buy</Button>}>
+										Please login first!
+									</Modal>}> {/*adding a "Buy" button with the price*/}
+								{/*name and description: */}
+								<h5>{prod.name}</h5>
+								{prod.description}.
+							</Card>
+						</Col>)
+				});
 
-				return (
-					// sets the size of the card for each type of screen
-					<Col s={6} m={4} l={2} >
-						<Card className='medium' /*type of the card*/
-							header={<CardTitle image={prod.img_file}></CardTitle>} /*adding an image*/
-							actions={<Modal
-								header={prod.name}
-								trigger={<Button>{prod.price} Buy</Button>}>
-									{/*Pop-up window with more details*/}
-									<Row>
-										{/*Larger product picture*/}
-										<Col l={4}>
-											<MediaBox src={prod.img_file} caption="Product picture" width="200"/>
-										</Col>
-										{/*Product info*/}
-										<Col l={4}>
-											<h5>Description:</h5>
-											<p>{prod.description}</p>
-											<h5>Total price:</h5>
-											<p>{parseInt(prod.price, 10) * this.state.quantity}</p>
-											<h5></h5>
-											<Input type="number" label="Quantity" min="1" max="100" defaultValue="1"
-												onChange={(e) => {this.setState({quantity: e.target.value})} }/>
-										</Col>
-									</Row>
-									{/*Delete option*/}
-									<Row className="left">
-										<Button modal="close" onClick={this.createNewItem}>
-											Add to cart
-										</Button>
-									</Row>
-								</Modal>}> {/*adding a "Buy" button with the price*/}
-							{/*name and description: */}
-							<h5>{prod.name}</h5>
-							{prod.description}.
-						</Card>
-					</Col>)
-			});
+			// returns the list of cards
+			return (
+				<Row>
+					{productList}
+				</Row>
+			);
+		}
+		else{
+			// fills a list with cards with name, image, price and a short description about each product
+			let productList = products.map((prod, index) => {
+					this.prodId = prod.id;
+					this.prodName = prod.name;
+					this.prodImg = prod.img_file;
+					this.prodDescription = prod.description;
+					this.prodPrice = prod.price;
 
-		// returns the list of cards
-		return (
-			<Row>
-				{productList}
-			</Row>
-		);
+					return (
+						// sets the size of the card for each type of screen
+						<Col s={6} m={4} l={2} >
+							<Card className='medium' /*type of the card*/
+								header={<CardTitle image={prod.img_file}></CardTitle>} /*adding an image*/
+								actions={<Modal
+									header={prod.name}
+									trigger={<Button>{prod.price} Buy</Button>}>
+										{/*Pop-up window with more details*/}
+										<Row>
+											{/*Larger product picture*/}
+											<Col l={4}>
+												<MediaBox src={prod.img_file} caption="Product picture" width="200"/>
+											</Col>
+											{/*Product info*/}
+											<Col l={4}>
+												<h5>Description:</h5>
+												<p>{prod.description}</p>
+												<h5>Total price:</h5>
+												<p>{parseInt(prod.price, 10) * this.state.quantity}</p>
+												<h5></h5>
+												<Input type="number" label="Quantity" min="1" max="100" defaultValue="1"
+													onChange={(e) => {this.setState({quantity: e.target.value})} }/>
+											</Col>
+										</Row>
+										{/*Delete option*/}
+										<Row className="left">
+											<Button modal="close" onClick={this.createNewItem}>
+												Add to cart
+											</Button>
+										</Row>
+									</Modal>}> {/*adding a "Buy" button with the price*/}
+								{/*name and description: */}
+								<h5>{prod.name}</h5>
+								{prod.description}.
+							</Card>
+						</Col>)
+				});
+
+			// returns the list of cards
+			return (
+				<Row>
+					{productList}
+				</Row>
+			);
+		}
 	}
 
 	createNewItem(){
