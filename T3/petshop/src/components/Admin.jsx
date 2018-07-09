60e020f3d09eb9722783354a49ce920f19c924d0
Marcos Camargo <marcoscrcamargo@gmail.com>
2018-07-09 01:07:53 -0300

    is admin user

--- T3/petshop/src/components/Admin.jsx
+++ T3/petshop/src/components/Admin.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tabs, Tab, Table, Button, Modal, MediaBox, Row, Col} from 'react-materialize';
+import { Tabs, Tab, Table, Button, Modal, MediaBox, Row, Col, Input} from 'react-materialize';
 
 import ProductsManagement from './ProductsManagement.jsx';
 import ServicesManagement from './ServicesManagement.jsx';
@@ -15,7 +15,7 @@ export default class Admin extends React.Component {
 			sales: [],
 		};
 
-
+		this.userToEdit = null;
 		this.handleIsAdmin = this.handleIsAdmin.bind(this);
 
 		// getting values from the db and assigning them to the users, products and services attributes
@@ -27,8 +27,10 @@ export default class Admin extends React.Component {
 		let users = this.state.users;
 		let sales = this.state.sales;
 
+
 		// for each user in users, runs the function
 		let usersTable = users.map((user, index) => {
+			let isAdminInput = user.value.admin === 'true' ? <Input name='isadmin' type='checkbox' value='admin' label='Admin' checked onChange={(e) =>{this.userToEdit = user; this.handleIsAdmin(e);}}/> : 									<Input name='isadmin' type='checkbox' value='admin' label='Admin' onChange={(e) =>{this.userToEdit = user; this.handleIsAdmin(e);}}/>;
 			return (
 				// creates a table with colums: Name, Username and Details
 				<tr key={index}>
@@ -56,6 +58,8 @@ export default class Admin extends React.Component {
 									<p>{user.value.email}</p>
 									<h5>Address:</h5>
 									<p>{user.value.adress}</p>
+									<h5>Admin:</h5>
+									{isAdminInput}
 								</Col>
 							</Row>
 						</Modal>
@@ -149,10 +153,32 @@ export default class Admin extends React.Component {
 		)
 	}
 
-	handleIsAdmin(e){
-		console.log(e.target.value);
-	}
+	async handleIsAdmin(e){
+		let user = this.userToEdit;
+		console.log(user);
+		let eventvalue = e.target.checked;
+		let response = await fetch('http://localhost:4000/user/' + user.id);
+		let reference = await response.json();
 
+		user.value._rev = reference._rev;
+		user.value.admin = eventvalue.toString();
+		var url = 'http://127.0.0.1:4000/user/';
+		fetch(url, {
+			headers: {
+				'Content-type':'application/json'
+			},
+			method:'PUT',
+			body: JSON.stringify(user)
+		}).then(() => {
+			console.log(user)
+			if (user.value.admin === "true"){
+				window.Materialize.toast(user.value.name + "is admin!", 2000);
+			} else {
+				window.Materialize.toast(user.value.name + "isn't admin!", 2000);
+			}
+		}).catch(()=>{console.log('se fude rapaz')});
+
+	}
 	async getAllUsers(){
 		let response = await fetch('http://localhost:4000/user');
 		let users = await response.json();
